32a3040524938c4187653aed4f2a4f90203279cc
Joshua Munn <josh@thebloggerprogramme.com>
2019-12-11 14:27:51 +0000

    updates version string

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="sqlite_tables",
-    version="0.0.7",
+    version="0.0.8",
     author="Joshua Munn",
     author_email="joshamunn@gmail.com",
     description="Classes for sqlite3 interaction",
